f616df9034bc80cb512edb9e096d76aadb54213d
Ravikant Vishwakarma <ravikant.vishwakarma@techprescient.com>
2022-10-05 10:17:38 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+
+function hide() {
+	$('.callback-btn').css({ "display": 'block' })
+	$('.callback-form-wrapper').css({ "display": 'none' })
+}
+function show() {
+	$('.callback-btn').css({ "display": 'none' })
+	$('.callback-form-wrapper').css({ "display": 'block' })
+}
+
+$(document).ready(function () {
+
+
+	$(".navbar-collapse a").click(function () {
+		$(".navbar-collapse").collapse("hide");
+	});
+
+	$("#cr1").owlCarousel({
+		loop: true,
+		margin: 20,
+		autoWidth: true,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 3
+			},
+			1000: {
+				items: 3
+			}
+		}
+
+	});
+	$("#cr2").owlCarousel({
+		loop: true,
+		margin: 20,
+		dots: false,
+		autoWidth: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 3
+			},
+			1000: {
+				items: 5
+			}
+		}
+	});
+
+
+	$('.toggle-btn').click(function () {
+		$(this).toggleClass("fa-plus")
+		$(this).prev().toggleClass("clr")
+		$(this).parent().next().animate({ height: 'toggle' },300)
+	})
+
+	$('.about-para').Cuttr({
+		//options here
+		truncate: 'words',
+		length: 21
+	});
+
+	$('.testimonial-para').Cuttr({
+		//options here
+		truncate: 'words',
+		length: 11
+	});
+
+
+
+});
